888e183431285296d7b7e46d6632e163e8321993
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-07-21 12:10:03 +0530

    learned stls

---
+++ stls/stls-imp.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <list>
+#include <stack>
+#include <queue>
+
+
+#include <map>
+using namespace std;
+
+int main(){
+    // vector<datatype> name(size, val)
+    // list<datatype> name // push_back, push_front, pop_back, pop_front // doubly linked list
+    // deque<datatype> name // push_back, push_front, pop_back, pop_front //double ended queue
+    // pair<datatype1, datatype2> name //.first, .second
+
+
+    // stack<datatype> name // push(), top(), pop(), front(), empty()
+    // queue<datatype> name // push(), front(), pop(), empty()
+    // priority_queue<datatype> name //push(), top(), pop() //largest(ascending)
+    // priority_queue<int, vector<int>, greater<int>> name //push(), top(), pop() //smallest(descending)
+
+    // map<datatype1, datatype2> name // unique keys, insert(), count(), erase(), find(), empty()
+
+    return 0;
+}
